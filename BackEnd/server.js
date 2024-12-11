de74944b257e21cf9597af239c2cec9037643b13
Adam Durand <g00405974@atu.ie>
2024-12-11 17:01:43 +0000

    First Commit

---
+++ BackEnd/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://adamdurand:Zxukwb9Jv!9@datarep.sjr0e.mongodb.net/?retryWrites=true&w=majority&appName=datarep');
